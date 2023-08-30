792c7dd096921b270c1dd47ed9e1de93bebcad89
ChrisEFont <cfont.it@gmail.com>
2023-08-29 22:38:59 -0300

    agregamos ejercicio 4 guia 12

--- GUIA 12/Ejercicio4/src/ejercicio4/Ejercicio3.java
+++ GUIA 12/Ejercicio4/src/ejercicio4/Ejercicio3.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio3;
+package ejercicio4;
 
 import entity.Circulo;
 import entity.Rectangulo;
